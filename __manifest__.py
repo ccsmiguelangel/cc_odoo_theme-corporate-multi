6630bbe5ec725e0a39e8eb8efca88441fc12ca53
Miguel <miguelangel1074.gc@gmail.com>
2026-01-15 03:02:07 +0000

    init project

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Tema Maestro Corporativo Multi-Website',
+    'description': 'Tema único corporativo para múltiples sitios web de empresas.',
+    'version': '19.0.1.0.0',
+    'category': 'Theme/Creative',
+    'depends': ['website', 'website_sale', 'zones_company'],
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml',
+        'data/theme_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
